d0bdbdea1bfbcd176c8f8292d140bc4454925b57
eiji1010 <ri2005hoshi@gmail.com>
2025-07-08 21:58:44 +0900

    muxルーターを使用してHTTPハンドラーの設定を整理

--- main.go
+++ main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"github.com/Eiji1010/go-api/handlers"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDecimalHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDecimalHandler)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("Server start at port:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
