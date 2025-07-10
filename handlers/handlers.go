ae447318bfec813fa0690553582c16eae65098a8
eiji1010 <ri2005hoshi@gmail.com>
2025-07-10 21:57:10 +0900

    PostArticleHandlerのリクエストボディの読み取りとエラーハンドリングを強化

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/Eiji1010/go-api/models"
 	"github.com/gorilla/mux"
@@ -15,7 +16,26 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	article := models.Article1
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "cannot get content length\n", http.StatusBadRequest)
+		return
+	}
+	reqBodybuffer := make([]byte, length)
+
+	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
+		http.Error(w, "Fail to get response body\n", http.StatusBadRequest)
+		return
+	}
+	var reqArticle models.Article
+	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
+		return
+	}
+
+	defer req.Body.Close()
+
+	article := reqArticle
 	jsonData, err := json.Marshal(article)
 	if err != nil {
 		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
